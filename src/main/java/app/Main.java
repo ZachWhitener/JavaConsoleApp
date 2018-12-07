5caa4b6bc7e33e4f3aae4b42a2c2c83847832bae
Zach Whitener <zachwhitener94@gmail.com>
2018-12-07 14:32:50 -0500

    skeleton

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
